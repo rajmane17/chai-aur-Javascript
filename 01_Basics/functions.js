42667a760644d52a79ee8e8740224012e2a679b6
Raj_Mane <142428026+rajmane17@users.noreply.github.com>
2023-12-27 15:12:27 +0000

    learning functions

---
+++ 01_Basics/functions.js
@@ -0,0 +1,11 @@
+function addTwoNumbers(num1, num2) {
+    let result = num1 + num2
+    return result
+}
+
+// if we call the function like this then it will show nothing 
+// addTwoNumbers()
+
+
+// we can execute it this way
+console.log(addTwoNumbers(2,7))
